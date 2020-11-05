cb4f3076bad8cc78a013bfa19da682b54aa96baa
Fredohm <fredwaucquez@hotmail.fr>
2020-11-05 05:14:50 +0100

    Initial commit

---
+++ src/main/java/org/fredohm/springbootintranet/SpringbootintranetApplication.java
@@ -0,0 +1,13 @@
+package org.fredohm.springbootintranet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootintranetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootintranetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fredohm/springbootintranet/SpringbootintranetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fredohm.springbootintranet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootintranetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
