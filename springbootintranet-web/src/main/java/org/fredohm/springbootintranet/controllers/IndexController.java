34e81b5339614ba9dd467f360d72cbe1013c4a3e
Fredohm <fredwaucquez@hotmail.fr>
2020-11-05 08:57:26 +0100

    Added CrudService interface. closes #3

--- springbootintranet-web/src/main/java/org/fredohm/springbootintranet/controllers/IndexController.java
+++ springbootintranet-web/src/main/java/org/fredohm/springbootintranet/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package org.fredohm.springbootintranet.controller;
+package org.fredohm.springbootintranet.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
