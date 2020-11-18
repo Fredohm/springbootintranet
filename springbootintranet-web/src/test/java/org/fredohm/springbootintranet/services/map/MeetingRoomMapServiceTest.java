3e659012348792f2f0c8289420b14b54babf325b
fredohm <fredwaucquez@hotmail.fr>
2020-11-18 16:24:41 +0100

    Creating test for Map Services in progress. #23

---
+++ springbootintranet-web/src/test/java/org/fredohm/springbootintranet/services/map/MeetingRoomMapServiceTest.java
@@ -0,0 +1,43 @@
+package org.fredohm.springbootintranet.services.map;
+
+import org.fredohm.springbootintranet.domain.MeetingRoom;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MeetingRoomMapServiceTest {
+
+    MeetingRoomMapService meetingRoomMapService;
+    final Long meetingRoomId = 1L;
+
+    @BeforeEach
+    void setUp() {
+        meetingRoomMapService = new MeetingRoomMapService();
+
+    }
+
+    @Test
+    void findAll() {
+        Set<MeetingRoom> meetingRooms = meetingRoomMapService.findAll();
+        assertEquals(1, meetingRooms.size());
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
