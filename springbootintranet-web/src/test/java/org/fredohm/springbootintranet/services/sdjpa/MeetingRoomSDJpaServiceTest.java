acd330f09935f65c728860907361d36a88802332
fredohm <fredwaucquez@hotmail.fr>
2020-11-19 14:32:24 +0100

    Creating tests for SD Jpa Services. In progress. #24

--- springbootintranet-web/src/test/java/org/fredohm/springbootintranet/services/sdjpa/MeetingRoomSDJpaServiceTest.java
+++ springbootintranet-web/src/test/java/org/fredohm/springbootintranet/services/sdjpa/MeetingRoomSDJpaServiceTest.java
@@ -1,22 +1,60 @@
 package org.fredohm.springbootintranet.services.sdjpa;
 
+import org.fredohm.springbootintranet.domain.MeetingRoom;
+import org.fredohm.springbootintranet.repositories.MeetingRepository;
+import org.fredohm.springbootintranet.repositories.MeetingRoomRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class MeetingRoomSDJpaServiceTest {
+
+    @Mock
+    MeetingRoomRepository meetingRoomRepository;
+
+    @Mock
+    MeetingRepository meetingRepository;
+
+    @InjectMocks
+    MeetingRoomSDJpaService service;
+
+    MeetingRoom returnMeetingRoom;
 
     @BeforeEach
     void setUp() {
+        returnMeetingRoom = MeetingRoom.builder().id(1L).build();
     }
 
     @Test
     void findAll() {
+        Set<MeetingRoom> returnMeetingRooms = new HashSet<>();
+        returnMeetingRooms.add(MeetingRoom.builder().id(1L).build());
+        returnMeetingRooms.add(MeetingRoom.builder().id(2L).build());
+        when(meetingRoomRepository.findAll()).thenReturn(returnMeetingRooms);
+
+        Set<MeetingRoom> meetingRooms = service.findAll();
+        assertNotNull(meetingRooms);
+        assertEquals(2, meetingRooms.size());
     }
 
     @Test
     void findById() {
+        when(meetingRoomRepository.findById(anyLong())).thenReturn(Optional.of(returnMeetingRoom));
+
+        MeetingRoom meetingRoom = service.findById(1L);
+        assertNotNull(meetingRoom);
     }
 
     @Test
@@ -29,5 +67,25 @@ class MeetingRoomSDJpaServiceTest {
 
     @Test
     void deleteById() {
+    }
+
+    @Test
+    void testFindAll() {
+    }
+
+    @Test
+    void testFindById() {
+    }
+
+    @Test
+    void testSave() {
+    }
+
+    @Test
+    void testDelete() {
+    }
+
+    @Test
+    void testDeleteById() {
     }
 }
