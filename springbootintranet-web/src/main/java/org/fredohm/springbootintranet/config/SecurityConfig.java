15f2f2142936894d7c656775373c5da356764069
Fredohm <fredwaucquez@hotmail.fr>
2021-01-12 07:20:19 +0100

    Add close and fade for logout message.

--- springbootintranet-web/src/main/java/org/fredohm/springbootintranet/config/SecurityConfig.java
+++ springbootintranet-web/src/main/java/org/fredohm/springbootintranet/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                     .loginPage("/login")
                     .loginProcessingUrl("/authenticateUser")
+                    .defaultSuccessUrl("/index", true)
                 .and()
                 .logout()
                     .logoutUrl("/doLogout")
