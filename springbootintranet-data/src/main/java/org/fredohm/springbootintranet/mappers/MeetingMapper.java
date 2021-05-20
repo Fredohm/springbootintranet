dceb92b504deb973da552d5ee0b52e9402ec829c
Fredohm <fredwaucquez@hotmail.fr>
2021-05-20 17:02:08 +0200

    suppression des 2 DTO's inutiles. (MeetingRoomListDTO et MeetingListDTO).

--- springbootintranet-data/src/main/java/org/fredohm/springbootintranet/mappers/MeetingMapper.java
+++ springbootintranet-data/src/main/java/org/fredohm/springbootintranet/mappers/MeetingMapper.java
@@ -3,12 +3,9 @@ package org.fredohm.springbootintranet.mappers;
 import org.fredohm.springbootintranet.domain.Meeting;
 import org.fredohm.springbootintranet.model.MeetingDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(uses = DateMapper.class)
 public interface MeetingMapper {
-
-    MeetingMapper INSTANCE = Mappers.getMapper(MeetingMapper.class);
 
     MeetingDTO meetingToMeetingDTO(Meeting meeting);
 
