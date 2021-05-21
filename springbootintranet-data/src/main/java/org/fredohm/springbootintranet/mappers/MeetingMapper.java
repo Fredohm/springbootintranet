84375f3e8e5bddd9cf981facd71219917f06659c
Fredohm <fredwaucquez@hotmail.fr>
2021-05-21 16:17:47 +0200

    Annotations @Mapping dans Mapper.

--- springbootintranet-data/src/main/java/org/fredohm/springbootintranet/mappers/MeetingMapper.java
+++ springbootintranet-data/src/main/java/org/fredohm/springbootintranet/mappers/MeetingMapper.java
@@ -3,11 +3,16 @@ package org.fredohm.springbootintranet.mappers;
 import org.fredohm.springbootintranet.domain.Meeting;
 import org.fredohm.springbootintranet.model.MeetingDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(uses = DateMapper.class)
 public interface MeetingMapper {
 
+    @Mapping(target = "meetingRoomDTO", source = "meetingRoom")
+    @Mapping(target = "meetingRoomDTO.meetings", ignore = true)
     MeetingDTO meetingToMeetingDTO(Meeting meeting);
 
+    @Mapping(target = "meetingRoom", source = "meetingRoomDTO")
+    @Mapping(target = "meetingRoomDTO.meetings", ignore = true)
     Meeting meetingDtoToMeeting(MeetingDTO meetingDTO);
 }
